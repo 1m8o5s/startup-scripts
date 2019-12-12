b3645d9921e4a36603f5371cf4e6a220c3253154
Kolya <1m8o5s>
2019-12-12 13:55:41 +0200

    created scripts for installation docker-compose and mongodb

--- mysql_i.sh
+++ mysql_i.sh
@@ -2,4 +2,28 @@
 
 sudo apt update
 sudo apt install -y mysql-server
-yes 123456 | sudo mysql_secure_installation
+
+sudo apt-get install -y expect
+
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"New password:\"
+send \"123123123\r\"
+expect \"Re-enter new password:\"
+send \"123123123\r\"
+expect \"Do you wish to continue with the password provided?(Press y|Y for Yes, any other key for No) :\"
+send \"y\r\"
+expect \"Remove anonymous users? (Press y|Y for Yes, any other key for No) :\"
+send \"y\r\"
+expect \"Disallow root login remotely? (Press y|Y for Yes, any other key for No) :\"
+send \"n\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+echo "$SECURE_MYSQL"
+sudo apt-get remove -y expect
+sudo systemctl enable mysql
