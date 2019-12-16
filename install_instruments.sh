81f1d06bf55c47d616f3431238befd43a3ec87c9
Kolya <1m8o5s>
2019-12-16 14:06:58 +0200

    Fix docker-compose installation

--- install_instruments.sh
+++ install_instruments.sh
@@ -14,7 +14,7 @@ else
 			sudo usermod -aG docker $USER
 		elif [[ $i = "compose" ]]
 		then
-			sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-com$
+			sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 			sudo chmod +x /usr/local/bin/docker-compose
 		elif [[ $i = "mysql" ]]
 		then
