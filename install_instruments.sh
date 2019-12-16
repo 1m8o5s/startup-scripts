72b0c98dd2910f8a42d0d7ab111646325578af84
Kolya <1m8o5s>
2019-12-16 13:46:57 +0200

    updated mongodb installation

--- install_instruments.sh
+++ install_instruments.sh
@@ -52,7 +52,7 @@ else
 			wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add - || \
 			sudo apt-get install gnupg && \
 			wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-			echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-or$
+			echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 			sudo apt-get update
 			sudo apt-get install -y mongodb-org
 			echo "mongodb-org hold" | sudo dpkg --set-selections
